f17575964d91aa3c4922e8d8828038d22f586329
Imam S Rifkan <imamsrifkan@gmail.com>
2023-09-05 20:25:35 +0700

    feat: Extends config jsx-a11y recommended

--- configs/recommended.ts
+++ configs/recommended.ts
@@ -4,6 +4,7 @@ const recommended = {
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
     "plugin:prettier/recommended",
   ],
   plugins: ["react", "react-hooks", "jsx-a11y", "prettier"],
